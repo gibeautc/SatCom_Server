321d4e628e8cfcf6c97d25ad231a0a272709f539
Chadg <gibeautc@oregonstate.edu>
2018-01-10 11:05:53 -0800

    update client

--- locClient.py
+++ locClient.py
@@ -3,11 +3,12 @@ import requests
 import logging as log
 import json
 from Crypto.PublicKey import RSA
-
+import time
+import sys
 print("Starting Client")
 
 log.basicConfig(filename="/home/pi/logs/locClient.log",level=log.DEBUG)
-
+log.info("Starting Client")
 def loadPub(filename):
     k=RSA.importKey(open(filename).read())
     return k
@@ -35,8 +36,49 @@ def get_data(key):
     encData=key.encrypt("MYID",32)
     r=requests.get("http://10.0.0.149:5050",data=encData[0])
     log.debug(r.text)
+def sendTempData():
+    f=open("/home/pi/therm/out.dat",'r')
+    dat=f.read()
+    jsonData={}
+    lines=dat.split('\n')
+    for line in lines:
+        if "a4ff" in line:
+            #inside
+            d=line.split(',')
+            d=d[1]
+            d=d.replace(")","")
+            jsonData['tempInSide']=float(d)
+        if "cc2ff" in line:
+            #outside
+            d=line.split(',')
+            d=d[1]
+            d=d.replace(")","")
+            jsonData['tempOutSide']=float(d)
+        if "23fff" in line:
+            #heatsink
+            d=line.split(',')
+            d=d[1]
+            d=d.replace(")","")
+            jsonData['hsTemp']=float(d)
+        if "SET" in line:
+            d=line.split(':')
+            jsonData['SET']=int(d[1])
+        if "STATUS" in line:
+            d=line.split(':')
+            jsonData['STATUS']=d[1]
+        #there is also notes 'NOTE' 
+    try:
+        r=requests.post("http://10.0.0.149:5000/loc",str(jsonData))
+        if r.status_code==200:
+            log.debug("Post-200")
+    except:
+        log.error("Failed to post data")
+        log.error(sys.exc_info())
 
 
-k=loadPub("RSA_Key_pub.pem")
+#k=loadPub("RSA_Key_pub.pem")
 #post_data("001",["data1","data2"],[1,2],k)
-get_data(k)
+#get_data(k)
+while True:
+    sendTempData()
+    time.sleep(60)
