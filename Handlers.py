d83143b53e36ece913b72617e8c99f12379dc79a
ChadG <gibeautc@oregonstate.edu>
2018-06-27 16:07:33 -0700

    update

--- Handlers.py
+++ Handlers.py
@@ -1,19 +1,58 @@
 #!/usr/bin/env python
 
 import logging
-import MySQLdb
-import time
+import urllib
 
-wxdb=MySQLdb.connect('localhost','root','aq12ws','weather')
-wxCurs=wxdb.cursor()
+def send_gm(message):
+	bot='0111eaa305c26110dd21040a0a'	#real
+	params=urllib.urlencode({'bot_id':bot,'text':message})
+	f=urllib.urlopen("https://api.groupme.com/v3/bots/post",params)
+	log.debug(f.read())
 
-def findLocation():
-	#need a valid loction ID from database, we have lat and long
-	#in request data. Pull all locations, check if there is one that has
-	#an acceptable distance   say 5 Miles?
-	#if we have one return the ID, if not we need to add it in (with rec of 1)
-	# and return new ID
-	pass
+def sat_message_rx(request):
+	#Message coming from sat. Could be an hourly update with or without attached message, or it could be priority message (location should be attached to that as well)
+	#assuming message has its own lat/lon   we will use that for groupme. If it doesnt, use the iridium cords but somehow show that its not accurate. 
+	#for now will be pushing all messages to groupme, but keep in mind we will want to add support to be able to send messages to another group (emergancy) or even to other platforms
+	#facebook, twitter, whatever
+
+	#For hourly/normal updates, once we have the location, we need to pull weather(hourly/daily) for this location, package up the data and send it back out to the sat
+	#have hook on SatCom Groupme, any message that starts with a a $ and then is less then 50 char will be sent. For now no restriction, but may want to think about that in the future. 
+	try:
+		data=request.form['data']
+		data=data.decode('hex')
+		momsn=request.form['momsn']
+		transmit_time=request.form['transmit_time']
+		iridium_lat=request.form['iridium_latitude']
+		iridium_lon=request.form['iridium_longitude']
+		ir_cep=request.form['iridium_cep']
+		logging.info("Message Number: "+str(momsn))
+		logging.info("Lat: "+str(iridium_lat))
+		logging.info("Lon: "+str(iridium_lon))
+		logging.info("Accuracy: "+str(ir_cep))
+		logging.info("Time: "+str(transmit_time))
+		logging.info("Message: "+str(data))
+		#first 4 bytes is lat, next 4 is lon then a byte of warnings and a byte of criticles 
+		#This is 10 bytes, if the messge is longer then everthing else is actual text
+		if len(msg)<10:
+			#This could be because a blank message is send when checking rx, or a bad message
+			logging.warning("Data is too short")
+			return	
+		else:
+			locData=msg[:10]
+			locData=bytearray.fromhex(locData)
+			gpsLat=struct.unpack('f', locData[:4])
+			gpsLon=struct.unpack('f', locData[4:8])
+			warn=locData[8]
+			crit=locData[9]
+			tmsg="Message Received from SatCom: "
+			if len(msg)>10:
+				tmsg=tmsg+str(msg[10:])
+			send_gm(tmsg)		
+			send_gm("Estimated Location:  Lat: "+str(iridium_lat)+"  Lon: "+str(iridium_lon))
+			send_gm("Actual Location:  Lat: "+str(gpsLat)+"  Lon: "+str(gpsLon))
+	except:
+		logging.error("POST Failed")
+
 def gm_message_rx(request):
 	try:
 		data=request.json
@@ -28,7 +67,6 @@ def gm_message_rx(request):
 		if sender!='bot':
 			logging.debug("Message is: "+message)
 			logging.debug("Message from: "+name)
-			logging.debug("In Group: "+groupID)
 		else:
 			return
 		if group_id.index(groupID)<2:
@@ -47,17 +85,18 @@ def gm_message_rx(request):
 				f=urllib.urlopen("https://core.rock7.com/rockblock/MT",params)
 				print(f.read())
 				logging.debug(str(f.read()))
-				#send_gm('Thanks Chad, your message of: "'+DATA+'" has been sent to the que')
+				send_gm('Thanks Chad, your message of: "'+DATA+'" has been sent to the que')
 			else:
-				#not authorized
 				try:
 					name=name.split(" ")
 					name=name[0]
 				except:
 					name=name
-				#send_gm("Sorry "+name+" , you are not authorized to send messages at this time.....please fuck off")
-		#print("GM Message from: "+str(name)+" ----:"+str(message))
+				send_gm("Sorry "+name+" , you are not authorized to send messages at this time.....please fuck off")
 	except:
 		logging.debug("Process GM message Failed")
 		logging.debug(request.json)
 		logging.error(sys.exc_info())
+		
+if __name__=="__main__":
+	print("Testing Handlers")
--- web_service.py
+++ web_service.py
@@ -11,9 +11,8 @@ import urllib
 import filelock
 import logging
 import os
-from GM import *
 from Handlers import *
-
+from WeatherAPI import *
 
 if os.path.isdir("/home/pi"):
 	system="pi"
@@ -32,70 +31,17 @@ lock.acquire()
 
 
 app=Flask(__name__)
-@app.route('/gmbot1',methods=['GET','POST'])
+@app.route('/gmbot',methods=['GET','POST'])
 def gm1():
 	logging.debug("GM Bot 1")
 	gm_message_rx(request)
 	return "done",200
 	
-@app.route('/gmbot2',methods=['GET','POST'])
-def gm2():
-	logging.debug("GM Bot 2")
-	gm_message_rx(request)
-	return "done",200
-
-@app.route('/gmbot3',methods=['GET','POST'])
-def gm3():
-	logging.debug("GM Bot 3")
-	gm_message_rx(request)
-	return "done",200
-
 @app.route('/sat',methods=['GET','POST'])
-#Message coming from sat. Could be an hourly update with or without attached message, or it could be priority message (location should be attached to that as well)
-#assuming message has its own lat/lon   we will use that for groupme. If it doesnt, use the iridium cords but somehow show that its not accurate. 
-#for now will be pushing all messages to groupme, but keep in mind we will want to add support to be able to send messages to another group (emergancy) or even to other platforms
-#facebook, twitter, whatever
-
-#For hourly/normal updates, once we have the location, we need to pull weather(hourly/daily) for this location, package up the data and send it back out to the sat
-#have hook on SatCom Groupme, any message that starts with a a $ and then is less then 50 char will be sent. For now no restriction, but may want to think about that in the future. 
 def sat():
 	if request.method=='POST':
 		logging.debug("got a post at /sat")
-		try:
-			data=request.form['data']
-			data=data.decode('hex')
-			momsn=request.form['momsn']
-			transmit_time=request.form['transmit_time']
-			iridium_lat=request.form['iridium_latitude']
-			iridium_lon=request.form['iridium_longitude']
-			ir_cep=request.form['iridium_cep']
-			logging.info("Message Number: "+str(momsn))
-			logging.info("Lat: "+str(iridium_lat))
-			logging.info("Lon: "+str(iridium_lon))
-			logging.info("Accuracy: "+str(ir_cep))
-			logging.info("Time: "+str(transmit_time))
-			logging.info("Message: "+str(data))
-			if str(data).startswith("$"):
-				data=data[1:]#strip off the leading $
-				dset=data.split(",")
-				Lat=dset[0]
-				Lon=dset[1]
-				Voltage=dset[2]
-				Status=dset[3]
-				#need to add bot info to these calls
-				#send_gm("Update:\rLat: "+str(Lat)+"\rLon: "+str(Lon)+"\rVoltage: "+str(Voltage)+"\rStatus: "+str(Status))
-				#send_gm("http://maps.google.com/maps?q="+str(Lat)+","+str(Lon))
-			elif str(data)=="":
-				#I think a blank message is sent when tring to retreve a message
-				#either way, no need to send a blank message to groupme
-				return "done",200	
-			else:
-				#need to add bot info to these calls
-				pass
-				#send_gm("Message Received from SatCom: "+str(data))		
-				#send_gm("Estimated Location:  Lat: "+str(iridium_lat)+"  Lon: "+str(iridium_lon))
-		except:
-			logging.error("POST Failed")
+		sat_message_rx(request)
 		return "done",200
 
 
